4ef6e3d1c1e63b844bedde0ba4c3434183bc47ce
Dubey411 <dubeytech9619@gmail.com>
2025-09-20 20:42:13 +0530

    Mistakes of SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
+
+
 import { useState } from "react";
+import * as Slider from "@radix-ui/react-slider";
 
 const Sidebar = () => {
-
   const deals = [
     { name: "Nike", count: 2 },
     { name: "Airmax", count: 48, active: true },
@@ -13,18 +15,17 @@ const Sidebar = () => {
   ];
 
   const colors = [
-    "#3b82f6", 
-    "#ef4444", 
-    "#000000", 
-    "#facc15", 
-    "#ec4899", 
-    "#f5f5f5", 
+    "#3b82f6",
+    "#ef4444",
+    "#000000",
+    "#facc15",
+    "#ec4899",
+    "#f5f5f5",
   ];
-
 
   const brands = [
     { name: "Nike", count: 99 },
-    { name: "Nike", count: 99, active:true},
+    { name: "Nike", count: 99, active: true },
     { name: "Adidas", count: 99 },
     { name: "Siemens", count: 99 },
   ];
@@ -32,15 +33,15 @@ const Sidebar = () => {
   const [selectedColor, setSelectedColor] = useState("#3b82f6");
 
 
+  const [priceRange, setPriceRange] = useState([13, 25]);
+
   return (
     <div className="absolute w-[355px] h-[1385px] top-[95.27px] left-[21.4px] opacity-100 space-y-6">
-      
+
       <div className="w-[349.23px] h-[548.57px] bg-[#F6F7F8] rounded-xl shadow-md p-5">
-   
         <h3 className="font-semibold font-[Poppins] text-[20px] leading-[100%] tracking-[0] text-[#22262A] mb-10">
           Hot Deals
         </h3>
-
         <ul className="space-y-9 leading-relaxed">
           {deals.map((deal, index) => (
             <li key={index} className="flex justify-between items-center">
@@ -53,7 +54,6 @@ const Sidebar = () => {
               >
                 {deal.name}
               </span>
-
               <span
                 className={
                   deal.active
@@ -68,19 +68,29 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="w-[347.94px] h-[190.35] bg-[#F6F7F8] rounded-xl shadow-md p-4 ">
-        <h3 className="font-proxima font-semibold text-[18px] mb-5">PRICES</h3>
+      <div className="w-[347.94px] h-[190.35px] bg-[#F6F7F8] rounded-xl shadow-md p-8">
+        <h3 className="font-proxima font-semibold text-[18px] mb-12">PRICES</h3>
         <div className="flex justify-between text-sm text-gray-600 mb-2">
-          <span>Ranger:</span>
-          <span>$13.99 - $25.99</span>
+          <span>Range:</span>
+          <span>
+            ${priceRange[0]}.99 - ${priceRange[1]}.99
+          </span>
         </div>
-        <input
-          type="range"
-          min="10"
-          max="50"
-          defaultValue="25"
-          className="w-full accent-blue-500"
-        />
+
+        <Slider.Root
+          className="relative flex items-center select-none w-full h-5"
+          value={priceRange}
+          onValueChange={setPriceRange}
+          min={10}
+          max={25}
+          step={1}
+        >
+          <Slider.Track className="bg-gray-300 relative grow rounded-full h-[6px]">
+            <Slider.Range className="absolute bg-blue-500 rounded-full h-full" />
+          </Slider.Track>
+          <Slider.Thumb className="block w-5 h-5 bg-blue-500 rounded-full shadow -translate-x-1/2" />
+          <Slider.Thumb className="block w-5 h-5 bg-blue-500 rounded-full shadow" />
+        </Slider.Root>
       </div>
 
       <div className="w-[347.94px] h-[144.22px] bg-[#F6F7F8] rounded-xl shadow-md p-6">
@@ -93,11 +103,11 @@ const Sidebar = () => {
               aria-label={`Select color ${c}`}
               onClick={() => setSelectedColor(c)}
               className={`w-7 h-7 rounded-full flex items-center justify-center transition 
-          ${
-            selectedColor === c
-              ? "ring-2 ring-green-500"
-              : "border-2 border-gray-300"
-          }`}
+                ${
+                  selectedColor === c
+                    ? "ring-2 ring-green-500"
+                    : "border-2 border-gray-300"
+                }`}
               style={{ backgroundColor: c }}
             />
           ))}
@@ -106,9 +116,8 @@ const Sidebar = () => {
 
       <div className="w-[347.94px] h-[303.15px] bg-[#F6F7F8] rounded-xl shadow-md p-6">
         <h3 className="font-proxima text-[18px] mb-6 font-semibold">BRAND</h3>
-      
         <ul className="space-y-8">
-           {brands.map((deal, index) => (
+          {brands.map((deal, index) => (
             <li key={index} className="flex justify-between items-center">
               <span
                 className={
@@ -119,7 +128,6 @@ const Sidebar = () => {
               >
                 {deal.name}
               </span>
-
               <span
                 className={
                   deal.active
@@ -133,7 +141,6 @@ const Sidebar = () => {
           ))}
         </ul>
       </div>
-
 
       <div className="w-[347.94px] h-[69.29px] bg-[#F6F7F8] rounded-xl shadow-md p-4 text-center cursor-pointer">
         <span className="text-gray-700 font-semibold font-[Poppins]">MORE</span>
