0557729242ea30502cfaf92e163c71385d919e21
Shubham <dubeytech9619@gmail.com>
2025-09-22 23:33:53 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-// import Icon from "../assets/Icon.png";
-// import Cart from "../assets/cart.png";
-
-// export default function Navbar() {
-//   return (
-//     <nav className="w-full h-[60px] flex items-center justify-between px-8 ">
-//       <div className="flex items-center gap-2">
-//         <img src={Icon} alt="Logo Icon" />
-
-//         <span className="font-poppins font-bold text-[24px] leading-[36px] text-[#22262A]">
-//           E-Comm
-//         </span>
-//       </div>
-
-//       <ul className="flex gap-[100px] font-poppins text-[16px] font-normal text-[#22262A]">
-//         <li className="text-[#40BFFF] cursor-pointer">HOME</li>
-//         <li className="hover:text-[#40BFFF] cursor-pointer">BAG</li>
-//         <li className="hover:text-[#40BFFF] cursor-pointer">SNEAKERS</li>
-//         <li className="hover:text-[#40BFFF] cursor-pointer">BELT</li>
-//         <li className="hover:text-[#40BFFF] cursor-pointer">CONTACT</li>
-//       </ul>
-
-//       <div className="flex items-center gap-2">
-//         <img
-//           src={Cart}
-//           alt="Cart"
-//          />
-//         <span className="font-poppins text-[16px] font-normal text-[#22262A]">
-//           Items
-//         </span>
-//         <span className="text-gray-500 font-proxima text-[16px]">$0.00</span>
-//       </div>
-//     </nav>
-//   );
-// }
 
 
 export default function Navbar() {
